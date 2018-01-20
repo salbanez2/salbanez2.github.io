80babb9213bc7b7a15a6cda2ea2085e2e0d46e5a
Savannah <sealbanez@gmail.com>
2018-01-19 21:03:29 -0600

    Started JS questioning system on failure page

---
+++ javascript/failure.js
@@ -0,0 +1,89 @@
+var score = 0
+var ahh = "EPIC FAIL!!!!";
+	alert(ahh);
+var lol = "Lol, just kidding! It'll probably be fine, but let's just check.";
+	alert(lol);
+var answer1= "One at a time, What did you feed me?";
+	prompt(answer1);
+	
+	if (answer1.toUppercase = almonds){
+		score += 10;
+	} else if (answer1.toUppercase = milk){
+		score += 20;
+	} else if (answer1.toUppercase = wheat) {
+		score += 30;
+	} else if (answer1.toUppercase = corn) {
+		score += 40;
+	} else if (answer1.toUppercase = onion) {
+		score += 50;
+	} else if (answer1.toUppercase = grapes) {
+		score += 60;
+	} else if (answer1.toUppercase = peaches) {
+		score += 70;
+	} else if (answer1.toUppercase = apricots) {
+		score += 80;
+	} else if (answer1.toUppercase = cashews) {
+		score += 90;
+	} else if (answer1.toUppercase = pecans) {
+		score += 90;
+	} else if (answer1.toUppercase = pears) {
+		score += 90;
+	} else {
+		score += 0;
+	}
+
+
+var answer2= "On a scale of 1-5 how much did you feed me?";
+	prompt(answer2)
+
+	if(answer2 ='1') {
+		score += 1;
+	} else if ('2'){
+		score += 2
+	} else if ('3') {
+		score += 3;
+	} else {
+		score += 4
+	}
+ 
+ if ( score  > 60) {
+ 		document.write (
+ 		"My ears and throat will get itchy, but that's about it. If there is enough for me to notice, I have probably quietly stopped eating already.");
+	} else if ( score  > 50) {
+		document.write ( 'I will not notice. I may keep eating. Please still tell me though. I need to keep track of my slip ups.');
+	} else if ( score  > 44) {
+		document.write ( "Corn is the worst for many reasons, but mostly because it is in everything. Additionally, I dont feel it in my ears until it's too late. Please tell me so that I can use my inhaler. If you have it, I want a benedryl. My ears are probably itching slightly, but I won't be sure why yet. Later, there will be a heavy pain in my chest. Im going to feel weak for the next day.");
+	} else if ( score  > 43) {
+		document.write ( "Corn is the worst for many reasons, but mostly because it is in everything. Additionally, I dont feel it in my ears until it's too late. Please tell me so that I can use my inhaler. I may go home and take a benedryl before it hurts. I'm going to feel weak for a few hours.");
+	} else if ( score  > 42) {
+		document.write ( " I won't notice for a little while, but when I do I will feel tight in the chest. I'll be uncomfortable for a bit, but I'll get over it.");
+	} else if ( score  > 41) {
+		document.write (" I am so not going to notice. Please still tell me though. I need to keep track of my slip ups.");
+	} else if ( score  > 34) {
+		document.write( "I need to take my inhaler and get some benedryl. I will probably head home. My ears are itchy and it feels weird when I swallow. When I notice I will stop eating, but it may be too late. Soon, I will lose all of my energy and become remarkably grumpy. My stomach will begin to hurt and I will feel weak for the next day.");
+	} else if ( score  > 33) {
+		document.write( "I need to take may need to take my inhaler. I may need some benedryl. When I notice I will stop eating, but it may be too late. I am gonna lose a lot of energy and probably get a bit more snarky. It's not you, it's the wheat. In a way though, it was you-lol.");
+	} else if ( score  > 30) {
+		document.write ( "I probably won't notice. Wheat does affect my energy levels, and my energy levels affect my crabiness levels. Watch out for added snark! And remember- you did this to you! Please tell me so I can start blaming you right away! LOL jk, I take full responsibilities for my action, regardless of wheat and it's bi-products.");
+	} else if ( score  > 23) {
+		document.write("The awesome thing about a milk allergy is that I typically notice right away. Also, It doesn't affect me nearly as badly as corn, wheat or almonds. Sometimes, I just power through a nice piece of cheese. That being said, I have probably already stopped eating or accepted the consequeces. Please still tell me. I will still take my inhaler.")
+	} else if ( score  > 20) {
+		document.write("The awesome thing about a milk allergy is that I typically notice right away. Also, It doesn't affect me nearly as badly as corn, wheat or almonds. Sometimes, I just power through a nice piece of cheese. That being said, with lower levels I may not notice so quickly. Please tell me so that I can make a judgment call. I like to save my mistakes for that nice cheese I was talking about.");
+	} else if ( score  > 14) {
+		document.write("Ok, please don't panic, but that is kind of a big whoops. I don't go into anaphylactic shock, so I mean that's way less scary. I may have noticed soon enough, but if not, I need to go home NOW. I need benedryl yesterday. I need my inhaler. I will be out of commission for two days. If we don't do these things quickly I will get hives.");
+	} else if ( score  > 13) {
+		document.write("Ok, please don't panic, but that is kind of a big whoops. I don't go into anaphylactic shock, so I mean that's way less scary. I may have noticed soon enough, but if not, I need to go home now and I'll take a benedryl when I get there. I need my inhaler. I will be out of commission for maybe a day. If we don't do these things quickly I will get hives.");
+	} else if ( score  > 12) {
+		document.write("Almonds are the only allergen that have given me hives. At a lower level my throat and ears are probably pretty itchy and weird. Be sure on your levels, any higher and I need a benedryl. I should take my inhaler. I may go home.");
+ 	} else if ( score  > 10) {
+		document.write("Almonds are the only allergen that have given me hives. At a lower level my throat and ears are probably pretty itchy and weird. Be sure on your levels, at higher levels I need a benedryl. I should take my inhaler but I'll be fine.");
+	} else {
+		document.write("I'm fine, that's not on the list.");
+	}
+
+
+
+
+
+
+
