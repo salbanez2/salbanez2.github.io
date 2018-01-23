10883632ceb736559b119ffa5b87d507e24a1adf
Savannah <sealbanez@gmail.com>
2018-01-22 21:38:58 -0600

    Failure.css
    The footer on Failure.css has a left margin- next step maybe- integrate with index.css

---
+++ javascript/corn.js
@@ -0,0 +1,2 @@
+var message = "Sorry for the language, but corn is in everything. It is the actual worst.";
+	alert(message);
--- javascript/failure.js
+++ javascript/failure.js
@@ -47,40 +47,40 @@ var question2 = prompt("On a scale of 1-5 how much did you feed me?");
 
  if ( score  > 60) {
  		document.write (
- 		"<p>My ears and throat will get itchy, but that's about it. If there is enough for me to notice, I have probably quietly stopped eating already.</p>");
+ 		"<p>	My ears and throat will get itchy, but that's about it. If there is enough for me to notice, I have probably quietly stopped eating already.</p>");
 	} else if ( score  > 50) {
-		document.write ( '<p>I will not notice. I may keep eating. Please still tell me though. I need to keep track of my slip ups.</p>');
+		document.write ( '<p>	I will not notice. I may keep eating. Please still tell me though. I need to keep track of my slip ups.</p>');
 	} else if ( score  > 44) {
-		document.write ( "<p> Corn is the worst for many reasons, but mostly because it is in everything. Additionally, I dont feel it in my ears until it's too late. Please tell me so that I can use my inhaler. If you have it, I want a benedryl. My ears are probably itching slightly, but I won't be sure why yet. Later, there will be a heavy pain in my chest. Im going to feel weak for the next day.</p>");
+		document.write ( "<p>	Corn is the worst for many reasons, but mostly because it is in everything. Additionally, I don't feel it in my ears until it's too late. Please tell me so that I can use my inhaler. If you have it, I want a benedryl. My ears are probably itching slightly, but I won't be sure why yet. Later, there will be a heavy pain in my chest. Im going to feel weak for the next day.</p>");
 	} else if ( score  > 43) {
-		document.write ( "<p>Corn is the worst for many reasons, but mostly because it is in everything. Additionally, I dont feel it in my ears until it's too late. Please tell me so that I can use my inhaler. I may go home and take a benedryl before it hurts. I'm going to feel weak for a few hours.</p>");
+		document.write ( "<p>	Corn is the worst for many reasons, but mostly because it is in everything. Additionally, I don't feel it in my ears until it's too late. Please tell me so that I can use my inhaler. I may go home and take a benedryl before it hurts. I'm going to feel weak for a few hours.</p>");
 	} else if ( score  > 42) {
-		document.write ( "<p>I won't notice for a little while, but when I do I will feel tight in the chest. I'll be uncomfortable for a bit, but I'll get over it.</p>");
+		document.write ( "<p>	I won't notice for a little while, but when I do I will feel tight in the chest. I'll be uncomfortable for a bit, but I'll get over it.</p>");
 	} else if ( score  > 41) {
-		document.write ("<p>I am so not going to notice. Please still tell me though. I need to keep track of my slip ups.</p>");
+		document.write ("<p>	I am so not going to notice. Please still tell me though. I need to keep track of my slip ups.</p>");
 	} else if ( score  > 34) {
-		document.write( "<p>I need to take my inhaler and get some benedryl. I will probably head home. My ears are itchy and it feels weird when I swallow. When I notice I will stop eating, but it may be too late. Soon, I will lose all of my energy and become remarkably grumpy. My stomach will begin to hurt and I will feel weak for the next day.</p>");
+		document.write( "<p>	I need to take my inhaler and get some benedryl. I will probably head home. My ears are itchy and it feels weird when I swallow. When I notice I will stop eating, but it may be too late. Soon, I will lose all of my energy and become remarkably grumpy. My stomach will begin to hurt and I will feel weak for the next day.</p>");
 	} else if ( score  > 33) {
-		document.write( "<p>I need to take may need to take my inhaler. I may need some benedryl. When I notice I will stop eating, but it may be too late. I am gonna lose a lot of energy and probably get a bit more snarky. It's not you, it's the wheat. In a way though, it was you-lol.</p>");
+		document.write( "<p>	I need to take may need to take my inhaler. I may need some benedryl. When I notice I will stop eating, but it may be too late. I am gonna lose a lot of energy and probably get a bit more snarky. It's not you, it's the wheat. In a way though, it was you-lol.</p>");
 	} else if ( score  > 30) {
-		document.write ( "<p>I probably won't notice. Wheat does affect my energy levels, and my energy levels affect my crabiness levels. Watch out for added snark! And remember- you did this to you! Please tell me so I can start blaming you right away! LOL jk, I take full responsibilities for my action, regardless of wheat and it's bi-products.</p>");
+		document.write ( "<p>	I probably won't notice. Wheat does affect my energy levels, and my energy levels affect my crabiness levels. Watch out for added snark! And remember- you did this to you! Please tell me so I can start blaming you right away! LOL jk, I take full responsibilities for my action, regardless of wheat and it's bi-products.</p>");
 	} else if ( score  > 23) {
-		document.write("<p>The awesome thing about a milk allergy is that I typically notice right away. Also, It doesn't affect me nearly as badly as corn, wheat or almonds. Sometimes, I just power through a nice piece of cheese. That being said, I have probably already stopped eating or accepted the consequeces. Please still tell me. I will still take my inhaler.</p>");
+		document.write("<p>	The awesome thing about a milk allergy is that I typically notice right away. Also, It doesn't affect me nearly as badly as corn, wheat or almonds. Sometimes, I just power through a nice piece of cheese. That being said, I have probably already stopped eating or accepted the consequeces. Please still tell me. I will still take my inhaler.</p>");
 	} else if ( score  > 20) {
-		document.write("<p>The awesome thing about a milk allergy is that I typically notice right away. Also, It doesn't affect me nearly as badly as corn, wheat or almonds. Sometimes, I just power through a nice piece of cheese. That being said, with lower levels I may not notice so quickly. Please tell me so that I can make a judgment call. I like to save my mistakes for that nice cheese I was talking about.</p>");
+		document.write("<p>	The awesome thing about a milk allergy is that I typically notice right away. Also, It doesn't affect me nearly as badly as corn, wheat or almonds. Sometimes, I just power through a nice piece of cheese. That being said, with lower levels I may not notice so quickly. Please tell me so that I can make a judgment call. I like to save my mistakes for that nice cheese I was talking about.</p>");
 	} else if ( score  > 14) {
-		document.write("<p>Ok, please don't panic, but that is kind of a big whoops. I don't go into anaphylactic shock, so I mean that's way less scary. I may have noticed soon enough, but if not, I need to go home NOW. I need benedryl yesterday. I need my inhaler. I will be out of commission for two days. If we don't do these things quickly I will get hives.</p>");
+		document.write("<p>	Ok, please don't panic, but that is kind of a big whoops. I don't go into anaphylactic shock, so I mean that's way less scary. I may have noticed soon enough, but if not, I need to go home NOW. I need benedryl yesterday. I need my inhaler. I will be out of commission for two days. If we don't do these things quickly I will get hives.</p>");
 	} else if ( score  > 13) {
-		document.write("<p>Ok, please don't panic, but that is kind of a big whoops. I don't go into anaphylactic shock, so I mean that's way less scary. I may have noticed soon enough, but if not, I need to go home now and I'll take a benedryl when I get there. I need my inhaler. I will be out of commission for maybe a day. If we don't do these things quickly I will get hives.</p>");
+		document.write("<p>	Ok, please don't panic, but that is kind of a big whoops. I don't go into anaphylactic shock, so I mean that's way less scary. I may have noticed soon enough, but if not, I need to go home now and I'll take a benedryl when I get there. I need my inhaler. I will be out of commission for maybe a day. If we don't do these things quickly I will get hives.</p>");
 	} else if ( score  > 12) {
-		document.write("<p>Almonds are the only allergen that have given me hives. At a lower level my throat and ears are probably pretty itchy and weird. Be sure on your levels, any higher and I need a benedryl. I should take my inhaler. I may go home.</p>");
+		document.write("<p>	Almonds are the only allergen that have given me hives. At a lower level my throat and ears are probably pretty itchy and weird. Be sure on your levels, any higher and I need a benedryl. I should take my inhaler. I may go home.</p>");
  	} else if ( score  > 10) {
-		document.write("<p>Almonds are the only allergen that have given me hives. At a lower level my throat and ears are probably pretty itchy and weird. Be sure on your levels, at higher levels I need a benedryl. I should take my inhaler but I'll be fine.</p>");
+		document.write("<p>	Almonds are the only allergen that have given me hives. At a lower level my throat and ears are probably pretty itchy and weird. Be sure on your levels, at higher levels I need a benedryl. I should take my inhaler but I'll be fine.</p>");
 	} else {
-		document.write("<p>I'm fine, that's not on the list.</p>");
+		document.write("<p>	I'm fine, that's not on the list.</p>");
 	}
 
-
+document.write('<p>Refresh the page to test another allergen.</p>')
 
 
 
